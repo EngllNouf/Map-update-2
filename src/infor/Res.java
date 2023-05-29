c34bfefd0a6ac7f97a2eeefa638c82479e70c00a
EngllNouf <124361377+EngllNouf@users.noreply.github.com>
2023-05-29 17:23:20 +0300

    GuideKSA

--- src/infor/Res.java
+++ src/infor/Res.java
@@ -1,194 +1,168 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication13;
+package infor;
 
-/**
- *
- * @author alhar
- */
-
-import java.awt.Color;
-import java.awt.Desktop;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import javax.swing.*;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
 
 public class Res extends JFrame {
-    private JLabel nameLabel,AgeLabel,emailLabel;
-    private JTextField nameField,AgeField,EmailField;
-    private JButton checkButton;
-    private JLabel resultLabel;
-    private JRadioButton aloneRadioButton;
-    private JRadioButton groupRadioButton;
-    private JLabel groupSizeLabel;
+    private JLabel nameLabel, AgeLabel,genderLabel,phoneLabel,emailLabel;
+    private JTextField nameField, AgeField,genderField,phoneField ,EmailField;
+    private JButton checkButton, inviteFriendButton, submitButton;
+    private JLabel resultLabel, groupSizeLabel;
+    private JRadioButton aloneRadioButton, groupRadioButton;
     private JTextField groupSizeField;
     private JCheckBox randomGroupCheckbox;
-    private JComboBox neighborhood;
-    private JButton inviteTeamButton;
-    private JButton submitButton;
- 
-    private JPanel panel = new JPanel(new GridLayout(1, 6));
-    private JPanel panel2 = new JPanel(new GridLayout(6, 1));
-    private JPanel panel3 = new JPanel(new GridLayout(1, 6));
-    
+    private JComboBox<String> neighborhoodBox,genderComboBox;
+    private JPanel infoPanel, jsonPanel, groupPanel;
+
     public Res() {
-        
         // Set the layout of the frame to GridLayout
-        setLayout(new GridLayout(5, 1));
+        setLayout(new GridLayout(4, 2));
         
-        // Create the name label and text field components
-        nameLabel = new JLabel("Name:");
-        nameField = new JTextField();
-        AgeLabel = new JLabel("Age:");
-        AgeField = new JTextField();
-        emailLabel = new JLabel("Email:");
-        EmailField = new JTextField(20);
-        
-        panel.add(nameLabel);
-        panel.add( nameField);
-        panel.add(AgeLabel);
-        panel.add(AgeField);
-        panel.add(emailLabel);
-        panel.add(EmailField);
-        // Create the radio button components
+        // Add the radio buttons to a button group
+        ButtonGroup buttonGroup = new ButtonGroup();
         aloneRadioButton = new JRadioButton("Going alone");
         groupRadioButton = new JRadioButton("Going with a group");
-        ButtonGroup buttonGroup = new ButtonGroup();
         buttonGroup.add(aloneRadioButton);
         buttonGroup.add(groupRadioButton);
-
-        // Create the group size label and text field components
+        groupRadioButton.addActionListener(e -> {
+            groupSizeField.setEnabled(true);
+            randomGroupCheckbox.setEnabled(true);
+            inviteFriendButton.setEnabled(true);
+            neighborhoodBox.setEnabled(true);
+        });
+        aloneRadioButton.addActionListener(e -> {
+            groupSizeField.setEnabled(true);
+            randomGroupCheckbox.setEnabled(false);
+            inviteFriendButton.setEnabled(false);
+            neighborhoodBox.setEnabled(false);
+        });
+        JPanel radioPanel = new JPanel(new GridLayout(2, 1));
+        radioPanel.add(aloneRadioButton);
+        radioPanel.add(groupRadioButton);
+        add(radioPanel);
+        
+        // Create the information panel and add the components to it
+        infoPanel = new JPanel(new GridLayout(5, 2));
+        String[] genderBox = {"Female", "Male"};
+        genderComboBox = new JComboBox<>(genderBox);
+        infoPanel.setBorder(BorderFactory.createCompoundBorder(
+        BorderFactory.createTitledBorder("Information"),
+        BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        Font newFont = new Font("Arial", Font.PLAIN, 12);
+        nameLabel = new JLabel("Name:");
+        nameLabel.setFont(newFont);
+        nameField = new JTextField();
+        AgeLabel = new JLabel("Age:");
+        AgeLabel.setFont(newFont);
+        AgeField = new JTextField();
+        genderLabel = new JLabel("Gender:");
+        genderLabel.setFont(newFont);
+        genderField = new JTextField();
+        phoneLabel = new JLabel("Phone Number:");
+        phoneLabel.setFont(newFont);
+        phoneField = new JTextField(10);
+        emailLabel = new JLabel("Email:");
+        emailLabel.setFont(newFont);
+        EmailField = new JTextField(20);
+        infoPanel.add(nameLabel);
+        infoPanel.add(nameField);
+        infoPanel.add(AgeLabel);
+        infoPanel.add(AgeField);
+        infoPanel.add(genderLabel);
+        infoPanel.add(genderComboBox);
+        checkButton = new JButton("Check");
+        //infoPanel.add(genderField);
+        infoPanel.add(phoneLabel);
+        infoPanel.add(phoneField);
+        infoPanel.add(emailLabel);
+        infoPanel.add(EmailField);
+        infoPanel.setBackground(Color.WHITE); // Set background color
+        
+        add(infoPanel);
+       
+        
+// Create the group panel and add the components to it
+        groupPanel = new JPanel(new GridLayout(6, 2));
+        groupPanel.setBorder(BorderFactory.createCompoundBorder(
+        BorderFactory.createTitledBorder("Group Information"),
+        BorderFactory.createEmptyBorder(10, 10, 10, 10)));
         groupSizeLabel = new JLabel("Group size:");
+        groupSizeLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         groupSizeField = new JTextField(4);
         groupSizeField.setEnabled(false);
-        // ذا حق المناطق انا خليت المناطق في كومبو بوكس في حال اختار اليوزر 
-        // Create the random group checkbox and invite team button components
         randomGroupCheckbox = new JCheckBox("Random group");
-        inviteTeamButton = new JButton("Invite team");
-        inviteTeamButton.setEnabled(false);
-        // ذا حق المناطق انا خليت المناطق في كومبو بوكس في حال اختار اليوزر الحي حقه يطلع له لنك باقرب منطقة 
-             JLabel neighborhoodLabel = new JLabel("Neighborhood:");
-            String[] neighborhoods = {"Al Zahra", "C", "D", "R", "p"};
-            JComboBox<String> neighborhood = new JComboBox<>(neighborhoods);
-            neighborhood.setEnabled(false);
-
-   // هنا قلنا انه اذا اختارخلاص سويتش لكل منطقة عشان يطلع له الرابط يمكن نقدر نسويها بفايل االله واعلم
-   neighborhood.addActionListener(e -> {
-    JComboBox<String> combo = (JComboBox<String>) e.getSource();
-    String selectedNeighborhood = (String) combo.getSelectedItem();
-    if (selectedNeighborhood != null) {
+        inviteFriendButton = new JButton("Invite friend");
+        inviteFriendButton.setEnabled(false);
+        String[] neighborhoods = {"Al Zahra", "Al Rawabi", "Al Safa", "Al Naseem", "Al Shatiea"};
+        neighborhoodBox = new JComboBox<>(neighborhoods);
+        neighborhoodBox.setEnabled(false);
+       
+        resultLabel = new JLabel();
+        JLabel linkLabel = new JLabel("link");
+        
+        // Add neighborhood links
+        neighborhoodBox.addActionListener(e -> {
+        JComboBox<String> combo = (JComboBox<String>) e.getSource();
+        String selectedNeighborhood = (String) combo.getSelectedItem();
+        if (selectedNeighborhood != null) {
         switch (selectedNeighborhood) {
             case "Al Zahra":
-                // الكود ذا حق االلنك عشان يشغله 
-  
-            JLabel linkLabel = new JLabel("link");
-            linkLabel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-            linkLabel.addMouseListener(new java.awt.event.MouseAdapter() {
-                public void mouseClicked(java.awt.event.MouseEvent evt) {
-                    try {
-                        Desktop.getDesktop().browse(new URI("https://goo.gl/maps/VxXXL1KPTEFbMN9S8"));
-                        JOptionPane.showMessageDialog(panel, "Link opened in browser.");
-                    } catch (IOException | URISyntaxException ex) {
-                        // handle exception
+                // Add link for Al Zahra neighborhood
+                linkLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                linkLabel.addMouseListener(new MouseAdapter() {
+                    public void mouseClicked(MouseEvent evt) {
+                        try {
+                            Desktop.getDesktop().browse(new URI("https://goo.gl/maps/VxXXL1KPTEFbMN9S8"));
+                            JOptionPane.showMessageDialog(infoPanel, "Link opened in browser.");
+                        } catch (IOException | URISyntaxException ex) {
+                            // Handle the exception
+                        }
                     }
-                }
-            });
-
-panel.add(linkLabel);
-
-      
-            // باقي الاحياء هنا
+                });
                 break;
-            case "C":
-                JOptionPane.showMessageDialog(this, "You selected C.");
+            case "Al Rawabi":
+                // Add link for C neighborhood
                 break;
-            case "D":
-                JOptionPane.showMessageDialog(this, "You selected D.");
+            case "Al Safa":
+                // Add link for D neighborhood
                 break;
-            case "R":
-                JOptionPane.showMessageDialog(this, "You selected R.");
+            case "Al Naseem":
+                // Add link for R neighborhood
                 break;
-            case "p":
-                JOptionPane.showMessageDialog(this, "You selected p.");
+            case "Al Shatiea":
+                // Add link for p neighborhood
                 break;
             default:
-                JOptionPane.showMessageDialog(this, "Unknown selection.");
+                JOptionPane.showMessageDialog(groupPanel, "Unknown selection.");
                 break;
         }
     }
 });
-
-
-        // Create the submit button component
-        submitButton = new JButton("Submit");
-
-        // Create the group panel and add the components to it
-           panel2 = new JPanel();
-           panel2.add(groupSizeLabel);
-           panel2.add(groupSizeField);
-           panel2.add(randomGroupCheckbox);
-           panel2.add(inviteTeamButton);
-
-        checkButton = new JButton("Check");
-        resultLabel = new JLabel();
-        
-        // هذا عشان يتاكد من صيغة الايميل الميثود حقته تحت 
-        checkButton.addActionListener(new ActionListener() {
-            
-            public void actionPerformed(ActionEvent e) {
-                String email = EmailField.getText();
-                if (isValidEmail(email)) {
-                    resultLabel.setText("Valid email syntax.");
-                    resultLabel.setForeground(Color.GREEN);
-                } else {
-                    resultLabel.setText("Invalid email syntax.");
-                    resultLabel.setForeground(Color.RED);
-                }
-            }
-        });
-     
-
-        panel.add(neighborhoodLabel);
-        panel.add(neighborhood);
-        panel.add(checkButton);
-        panel.add(resultLabel);
-        add(aloneRadioButton);
-        add(groupRadioButton);
-        add(panel);
-        add(panel2);
-         
-        
-        // هنا عشان اذا اليوزر اختار يبغى يروح لوحده او مع قروب في خيارات ما تطلع اذا راح لوحده هنا نخفي الي ما نبغاها يختارو
-        // Add an action listener to the group radio button to enable/disable the group size field and other components
-        groupRadioButton.addActionListener(e -> {
-            groupSizeField.setEnabled(true);
-            randomGroupCheckbox.setEnabled(true);
-            inviteTeamButton.setEnabled(true);
-            neighborhood.setEnabled(true);
-        });
-        aloneRadioButton.addActionListener(e -> {
-            groupSizeField.setEnabled(false);
-            randomGroupCheckbox.setEnabled(false);
-            inviteTeamButton.setEnabled(false);
-          
-        });
+        groupPanel.add(groupSizeLabel);
+        groupPanel.add(groupSizeField);
+        groupPanel.add(randomGroupCheckbox);
+        groupPanel.add(inviteFriendButton);
+        groupPanel.add(neighborhoodBox);
+      
+        groupPanel.add(linkLabel);
+        groupPanel.add(resultLabel);
+        add(groupPanel);
 
         // Add an action listener to the invite team button to handle inviting a team
-        inviteTeamButton.addActionListener(e -> {
+        inviteFriendButton.addActionListener(e -> {
             // Code to invite a team
-            JOptionPane.showMessageDialog(this, "Inviting team...");
+            JOptionPane.showMessageDialog(this, "Inviting friend...");
         });
 
         // Add an action listener to the submit button to handle the reservation submission
+        submitButton = new JButton("Submit");
         submitButton.addActionListener(e -> {
             String name = nameField.getText();
             boolean goingAlone = aloneRadioButton.isSelected();
@@ -198,12 +172,10 @@ panel.add(linkLabel);
                 try {
                     groupSize = Integer.parseInt(groupSizeField.getText());
                 } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(this, "Please enter a valid group size.");
+                    JOptionPane.showMessageDialog(this, "Please enter a validgroup size.");
                     return;
                 }
-            if(randomGroup)   
                 randomGroup = randomGroupCheckbox.isSelected();
-                
             }
             // Do something with the reservation information, such as store it in a database
             if (goingAlone) {
@@ -211,23 +183,16 @@ panel.add(linkLabel);
             } else {
                 String groupType = randomGroup ? "random group" : "invited team";
                 System.out.println("Reservation: " + name + ", going with a group of " + groupSize + " (type: " + groupType + ")");
-                 
             }
-            ;
         });
+        groupPanel.add(submitButton);
 
         // Set other properties of the frame
         setTitle("Reservation");
-        setSize(500, 500);
+        setSize(600, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-    }
-
-// حق الايميل 
-     public boolean isValidEmail(String email) {
-        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-       return email.matches(regex);
     }
     public static void main(String[] args) {
         new Res();
